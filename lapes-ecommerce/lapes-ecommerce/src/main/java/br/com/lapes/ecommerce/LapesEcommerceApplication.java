9b7619933bbb23e6dfa935d4b4b81e8f0084288a
PauloRCunhaDev <pauloricardoctec@gmail.com>
2026-05-20 16:49:53 -0300

    feat: initialize lapes-ecommerce project with Spring Boot setup

---
+++ lapes-ecommerce/lapes-ecommerce/src/main/java/br/com/lapes/ecommerce/LapesEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.lapes.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapesEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapesEcommerceApplication.class, args);
+	}
+
+}
---
+++ lapes-ecommerce/lapes-ecommerce/src/test/java/br/com/lapes/ecommerce/LapesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lapes.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapesEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
